d4baf2a8ceeb3eb4e7771192c251605dd474d064
GISpjd <3232606740@qq.com>
2023-10-17 15:29:25 +0800

    架构更新

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 function getLocation() {
     const x = document.getElementById("demo");
     try {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        window.navigator.geolocation.getCurrentPosition(showPosition);
     } catch (err) {
         x.innerHTML = err;
     }
@@ -15,3 +15,27 @@ function showPosition(position) {
         "Latitude: " + position.coords.latitude +
         "<br>Longitude: " + position.coords.longitude;
 }
+
+// const links = document.querySelectorAll('.link');
+// // 获取内容元素
+// const content = document.querySelector('#content');
+
+// // 添加点击事件处理程序
+// links.forEach(link => {
+//     link.addEventListener('click', (event) => {
+//         // 阻止默认行为
+//         event.preventDefault();
+
+//         // 发送 AJAX 请求获取页面内容
+//         const url = link.getAttribute('href');
+//         const xhr = new XMLHttpRequest();
+//         xhr.open('GET', url, true);
+//         xhr.onreadystatechange = function () {
+//             if (xhr.readyState === 4 && xhr.status === 200) {
+//                 // 更新内容
+//                 content.innerHTML = xhr.responseText;
+//             }
+//         }
+//         xhr.send();
+//     });
+// });
